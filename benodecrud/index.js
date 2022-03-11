b326842d9bc9abcf12f2de67347444f6ba009b8a
Will Worth <will.worth@ingka.com>
2022-03-11 14:29:53 +0100

    g

--- benodecrud/index.js
+++ benodecrud/index.js
@@ -1,6 +1,42 @@
+import express from 'express';
+import mysql from "mysql"
 import chalk from 'chalk';
 
+
+
+const port = 3000
 const log = console.log;
+
+// Create connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'nodemysql'
+});
+
+// Connect
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('MySql Connected...');
+});
+
+const app = express();
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World! from catalog')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
 // Combine styled and normal strings
 log(chalk.blue('Hello') + ' World' + chalk.red('!'));
