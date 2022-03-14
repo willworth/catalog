aa3a27c5ca4d1524dfeaa6a7ebc53177b014bdb5
Will Worth <will.worth@ingka.com>
2022-03-14 17:14:17 +0100

    connection

--- benodecrud/index.js
+++ benodecrud/index.js
@@ -1,43 +1,54 @@
 import express from 'express';
-import mysql from "mysql"
+import mysql from "mysql2"
 import chalk from 'chalk';
 
 
 
-const port = 3000
+const port = 7777
 const log = console.log;
 
 // Create connection
 const db = mysql.createConnection({
-    host: 'localhost',
+    // host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
-    password: '123456',
-    database: 'nodemysql'
+    password: 'testpassword',
+    database: 'monday'
 });
 
-// Connect
+// // Connect
 db.connect((err) => {
     if (err) {
         throw err;
     }
-    console.log('MySql Connected...');
+    log('MySql Connected...');
 });
 
 const app = express();
 
 
+// Create table
+    // app.get('/createcountrysettingstable', (req, res) => {
+    //     // let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))';
+    //     let sql = 'CREATE TABLE `country_settings` (`id` int(11) NOT NULL AUTO_INCREMENT,`lang` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,`price_per_meal` decimal(15, 2) DEFAULT NULL,`price_per_km` decimal(15, 2) DEFAULT NULL,`currency` char(15) COLLATE utf8_unicode_ci DEFAULT NULL,PRIMARY KEY(`id`)) ENGINE = InnoDB AUTO_INCREMENT = 6 DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci';
+    //     db.query(sql, (err, result) => {
+    //         if (err) throw err;
+    //         console.log(result);
+    //         res.send('country settings table created...');
+    //     });
+    // });
 
 
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World! from catalog')
+    res.send('Hello World! from catalog test app')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    log(chalk.blue.bgGreen.bold('Example app listening on port:'));
+    // console.log(`${port}`)
+    log(`${ chalk.black.bgYellow(port) }`);
 })
 
-// Combine styled and normal strings
-log(chalk.blue('Hello') + ' World' + chalk.red('!'));
 
